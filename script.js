7eb6680dc1c979fe3b43456d75c65a848f85aa8e
sethraiven <johan_sarbelaez@soy.sena.edu.co>
2025-02-18 17:19:58 -0500

    calculadoraMEJORADA

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-const botones = document.querySelectorAll('.numero');
-const operadores = document.querySelectorAll('.operador');
-const display = document.querySelector('#display');
-const borrar = document.querySelector('#borrar');
-const borrarTodo = document.querySelector('#borrarTodo');
-
-borrar.addEventListener('click', () => {
-    display.value = display.value.slice(0, -1);
-})
-
-borrarTodo.addEventListener('click', () => {
-    display.value = '';
-});
-
-const escribir = (event) => {
-    display.value += event.target.textContent;
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
 }
-
-botones.forEach(boton => {
-    boton.addEventListener('click', escribir);
-});
-
-
-
+function calculate() {
+    
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    
+}
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
