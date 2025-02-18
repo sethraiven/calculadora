83d84c1b06362b34c7460f56672d3c41894244af
sethraiven <johan_sarbelaez@soy.sena.edu.co>
2025-02-18 17:05:18 -0500

    JS

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const calbutons=document.querySelectorAll('.boton');
-const calvalue=document.querySelector("form input");
+const botones = document.querySelectorAll('.numero');
+const operadores = document.querySelectorAll('.operador');
+const display = document.querySelector('#display');
+const borrar = document.querySelector('#borrar');
+const borrarTodo = document.querySelector('#borrarTodo');
 
+borrar.addEventListener('click', () => {
+    display.value = display.value.slice(0, -1);
+})
+
+borrarTodo.addEventListener('click', () => {
+    display.value = '';
+});
+
+const escribir = (event) => {
+    display.value += event.target.textContent;
+}
+
+botones.forEach(boton => {
+    boton.addEventListener('click', escribir);
+});
 
 
 
