a59e64480875d8e94aa637beacc904c848e6616b
sethraiven <johan_sarbelaez@soy.sena.edu.co>
2025-02-25 16:09:37 -0500

    ultima actualización

--- script.js
+++ script.js
@@ -9,21 +9,3 @@ function calculate() {
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
-
-
-
-function squareRoot() {
-    document.getElementById('display').value = Math.sqrt(document.getElementById('display').value);
-}
-
-document.addEventListener('keydown', function(event) {
-    const key = event.key;
-    if (!isNaN(key) || ['+', '-', '*', '/', '.'].includes(key)) {
-        appendToDisplay(key);
-    } else if (key === 'Enter') {
-        calculate();
-   
-    } else if (key === 'Escape') {
-        clearDisplay();
-    }
-});
